ab961dd66428a8c16d3a169bf1622a0f4732ca2b
unknown <laurentiu.bondor@gmail.com>
2020-08-16 22:06:39 +0300

    first commit

---
+++ dom.js
@@ -0,0 +1,59 @@
+const name = document.getElementById("name");
+const lastname = document.getElementById("lastname");
+const form = document.getElementById("text-area");
+const button = document.querySelector('#button');
+
+
+
+button.addEventListener("click", sendForm);
+button.addEventListener("click", makeRed);
+button.addEventListener("click", showname);
+button.addEventListener("click", removeRed);
+button.addEventListener("click", removeRed2);
+button.addEventListener("click", genulmf);
+
+
+function makeRed() {
+    console.log('sa da click');
+    console.log("numele este " + name.value);
+
+    if (name.value && lastname.value) {
+        console.log('este valida');
+    } else {
+        console.log('introdu numele');
+        name.classList.add('invalid');
+        lastname.classList.add('invalid');
+
+
+    }
+}
+
+function sendForm() {
+    console.log("descrierea este " + form.value);
+}
+
+
+
+function showname() {
+    if (name.value && form.value)
+        var para = document.createElement("P");
+    para.innerHTML = "Thank you for contacting us." + name.value
+    document.getElementById("mydiv").appendChild(para);
+}
+
+function removeRed() {
+    if (name.value)
+        name.classList.remove("invalid")
+}
+
+function removeRed2() {
+    if (lastname.value)
+        lastname.classList.remove("invalid")
+}
+
+function genulmf() {
+
+    const genul = document.getElementsByName("gender")
+    console.log(genul.value)
+
+}
